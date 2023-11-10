efdfb1c30b92d2b4266ecc3f81b6b3c0a93820e7
Eric Long <i@hack3r.moe>
2023-11-10 16:25:32 +0800

    Naively optimize checksum calculation

--- src/bpf/checksum.h
+++ src/bpf/checksum.h
@@ -4,15 +4,16 @@
 #include <linux/bpf.h>
 #include <stddef.h>
 
+static __always_inline __u16 csum_fold(__u32 csum) {
+  csum = (csum & 0xffff) + (csum >> 16);
+  csum = (csum & 0xffff) + (csum >> 16);
+  return (__u16)~csum;
+}
+
 static void update_csum(__u16* csum, __s32 delta) {
   if (delta < 0) delta += 0xffff;
   __u32 new_csum = (__u16) ~*csum + delta;
-  for (int i = 0; i < 3; i++) {
-    __u16 hi = new_csum >> 16, lo = new_csum & 0xffff;
-    if (!hi) break;
-    new_csum = hi + lo;
-  }
-  *csum = ~new_csum;
+  *csum = csum_fold(new_csum);
 }
 
 static inline void update_csum_ul(__u16* csum, __u32 new) {
@@ -22,14 +23,17 @@ static inline void update_csum_ul(__u16* csum, __u32 new) {
 
 static void update_csum_data(struct __sk_buff* skb, __u16* csum, __u32 off) {
   __u16* data = (void*)(size_t)skb->data + off;
+  __u32 new_csum = (__u16) ~*csum;
   int i = 0;
   for (; i < ETH_DATA_LEN / sizeof(__u16); i++) {
     if ((size_t)(data + i + 1) > (size_t)skb->data_end) break;
-    update_csum(csum, bpf_ntohs(data[i]));
+    new_csum += bpf_ntohs(data[i]);
   }
   __u8* remainder = (__u8*)data + i * sizeof(__u16);
-  if ((size_t)(remainder + 1) > (size_t)skb->data_end) return;
-  update_csum(csum, (__u16)(*remainder << 8));
+  if ((size_t)(remainder + 1) > (size_t)skb->data_end) goto end;
+  new_csum += (__u16)(*remainder << 8);
+end:
+  *csum = csum_fold(new_csum);
 }
 
 #endif  // _MIMIC_BPF_CHECKSUM_H
