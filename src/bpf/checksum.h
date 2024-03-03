bf1f9f48188306dfc06babea6bc3759515a1204e
Eric Long <i@hack3r.moe>
2024-03-03 10:35:16 +0800

    Remove redundant folding in csum_fold

--- src/bpf/checksum.h
+++ src/bpf/checksum.h
@@ -6,7 +6,6 @@
 static inline u16 csum_fold(u32 csum) {
   csum = (csum & 0xffff) + (csum >> 16);
   csum = (csum & 0xffff) + (csum >> 16);
-  csum = (csum & 0xffff) + (csum >> 16);
   return (u16)~csum;
 }
 
