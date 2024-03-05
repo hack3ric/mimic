07531623a9551b70a26b609aefaea14f850c8e2d
Eric Long <i@hack3r.moe>
2024-03-05 23:17:37 +0800

    Add basic `mimic show`

--- src/show.c
+++ src/show.c
@@ -1,6 +1,10 @@
 #include <argp.h>
+#include <net/if.h>
+#include <stdio.h>
 
+#include "log.h"
 #include "mimic.h"
+#include "shared/util.h"
 
 static const struct argp_option show_args_options[] = {{}};
 
@@ -26,6 +30,18 @@ static inline error_t show_args_parse_opt(int key, char* arg, struct argp_state*
 const struct argp show_argp = {show_args_options, show_args_parse_opt, "INTERFACE", NULL};
 
 int subcmd_show(struct show_arguments* args) {
-  // TODO
+  int ifindex = if_nametoindex(args->ifname);
+  if (!ifindex) ret(1, "no interface named '%s'", args->ifname);
+
+  char lock[32];
+  snprintf(lock, sizeof(lock), "/run/mimic/%d.lock", ifindex);
+  FILE* lock_file = try_ptr(fopen(lock, "r"), "failed to open lock file at %s: %s", lock, strerrno);
+  struct lock_content lock_content;
+  try(lock_read(lock_file, &lock_content));
+  fclose(lock_file);
+
+  log_info("pid: %d", lock_content.pid);
+  // TODO: show filters and connections
+
   return 0;
 }
