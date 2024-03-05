2e8814ef275c0da08b3351a7fdb49f24a60a078b
Eric Long <i@hack3r.moe>
2024-03-05 23:17:26 +0800

    Filter out "exclusivity flag on" warning

--- src/log.c
+++ src/log.c
@@ -1,4 +1,5 @@
 #include <bpf/libbpf.h>
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "log.h"
@@ -6,18 +7,28 @@
 int log_verbosity = 2;
 
 int libbpf_print_fn(enum libbpf_print_level level, const char* format, va_list args) {
-  int result1;
+  int ret = 0;
   if (level == LIBBPF_WARN && LOG_ALLOW_WARN) {
-    result1 = fprintf(stderr, _LOG_WARN_PREFIX);
+    // Get rid of harmless warning when tc qdisc already exists
+    // This is dirty, but there is no other way to filter it
+    // See https://www.spinics.net/lists/bpf/msg44842.html
+    char buf[128];
+    ret = vsnprintf(buf, sizeof(buf), format, args);
+    if (ret < 0) return ret;
+    if (strstr(buf, "Exclusivity flag on, cannot modify")) {
+      return 0;
+    } else {
+      ret = fprintf(stderr, _LOG_WARN_PREFIX);
+      ret = ret < 0 ? ret : fprintf(stderr, "%s", buf);
+      return ret < 0 ? ret : 0;
+    }
   } else if (level == LIBBPF_INFO && LOG_ALLOW_INFO) {
-    result1 = fprintf(stderr, _LOG_INFO_PREFIX);
+    ret = fprintf(stderr, _LOG_INFO_PREFIX);
   } else if (level == LIBBPF_DEBUG && LOG_ALLOW_DEBUG) {
-    result1 = fprintf(stderr, _LOG_DEBUG_PREFIX);
+    ret = fprintf(stderr, _LOG_DEBUG_PREFIX);
   } else {
     return 0;
   }
-  if (result1 < 0) return result1;
-  int result2 = vfprintf(stderr, format, args);
-  if (result2 < 0) return result2;
-  return result1 + result2;
+  ret = ret < 0 ? ret : vfprintf(stderr, format, args);
+  return ret < 0 ? ret : 0;
 }
