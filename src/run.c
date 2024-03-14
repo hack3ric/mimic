d33fd126c5a9b245018f48084759bf199b787096
Eric Long <i@hack3r.moe>
2024-03-14 22:25:41 +0800

    Separate `parse_filter`

--- src/run.c
+++ src/run.c
@@ -106,48 +106,53 @@ static int handle_event(void* ctx, void* data, size_t data_sz) {
   return 0;
 }
 
+int parse_filter(const char* filter_str, struct pkt_filter* filter) {
+  char* delim_pos = strchr(filter_str, '=');
+  if (delim_pos == NULL || delim_pos == filter_str) {
+    ret(1, _("filter format should look like `{key}={value}`: %s"), filter_str);
+  }
+
+  if (strncmp("local=", filter_str, 6) == 0) {
+    filter->origin = ORIGIN_LOCAL;
+  } else if (strncmp("remote=", filter_str, 7) == 0) {
+    filter->origin = ORIGIN_REMOTE;
+  } else {
+    *delim_pos = '\0';
+    ret(1, _("unsupported filter type `%s`"), filter_str);
+  }
+
+  char* value = delim_pos + 1;
+  char* port_str = strrchr(value, ':');
+  if (!port_str) ret(-1, _("no port number specified: %s"), value);
+  *port_str = '\0';
+  port_str++;
+  char* endptr;
+  long port = strtol(port_str, &endptr, 10);
+  if (port <= 0 || port > 65535 || *endptr != '\0') ret(-1, _("invalid port number: `%s`"), port_str);
+  filter->port = htons((__u16)port);
+
+  int af;
+  if (strchr(value, ':')) {
+    if (*value != '[' || port_str[-2] != ']') {
+      ret(-1, _("did you forget square brackets around an IPv6 address?"));
+    }
+    filter->protocol = PROTO_IPV6;
+    value++;
+    port_str[-2] = '\0';
+    af = AF_INET6;
+  } else {
+    filter->protocol = PROTO_IPV4;
+    af = AF_INET;
+  }
+  if (inet_pton(af, value, &filter->ip.v6) == 0) ret(-1, _("bad IP address: %s"), value);
+  return 0;
+}
+
 static inline int parse_filters(struct run_arguments* args, struct pkt_filter* filters) {
   for (int i = 0; i < args->filter_count; i++) {
     struct pkt_filter* filter = &filters[i];
     char* filter_str = args->filters[i];
-    char* delim_pos = strchr(filter_str, '=');
-    if (delim_pos == NULL || delim_pos == filter_str) {
-      ret(1, _("filter format should look like `{key}={value}`: %s"), filter_str);
-    }
-
-    if (strncmp("local=", args->filters[i], 6) == 0) {
-      filter->origin = ORIGIN_LOCAL;
-    } else if (strncmp("remote=", args->filters[i], 7) == 0) {
-      filter->origin = ORIGIN_REMOTE;
-    } else {
-      *delim_pos = '\0';
-      ret(1, _("unsupported filter type `%s`"), filter_str);
-    }
-
-    char* value = delim_pos + 1;
-    char* port_str = strrchr(value, ':');
-    if (!port_str) ret(1, _("no port number specified: %s"), value);
-    *port_str = '\0';
-    port_str++;
-    char* endptr;
-    long port = strtol(port_str, &endptr, 10);
-    if (port <= 0 || port > 65535 || *endptr != '\0') ret(1, _("invalid port number: `%s`"), port_str);
-    filter->port = htons((__u16)port);
-
-    int af;
-    if (strchr(value, ':')) {
-      if (*value != '[' || port_str[-2] != ']') {
-        ret(1, _("did you forget square brackets around an IPv6 address?"));
-      }
-      filter->protocol = PROTO_IPV6;
-      value++;
-      port_str[-2] = '\0';
-      af = AF_INET6;
-    } else {
-      filter->protocol = PROTO_IPV4;
-      af = AF_INET;
-    }
-    if (inet_pton(af, value, &filter->ip.v6) == 0) ret(1, _("bad IP address: %s"), value);
+    try(parse_filter(filter_str, filter));
   }
   return 0;
 }
@@ -228,7 +233,7 @@ static inline int run_bpf(struct run_arguments* args, struct pkt_filter* filters
   try2(bpf_map__update_elem(skel->maps.mimic_settings, &vkey, sizeof(__u32), &vvalue, sizeof(__u32), BPF_ANY),
        _("failed to set BPF log verbosity: %s"), strerror(-_ret));
 
-  bool value = 1;
+  bool value = true;
   for (int i = 0; i < args->filter_count; i++) {
     retcode = bpf_map__update_elem(skel->maps.mimic_whitelist, &filters[i], sizeof(struct pkt_filter), &value,
                                    sizeof(bool), BPF_ANY);
