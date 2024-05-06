262194d7ba68c9da4906fc5671f5434d43399fe0
Eric Long <i@hack3r.moe>
2024-05-06 15:41:49 +0800

    kmod: remove crypto for now
    This commit may be reverted later if I decide to continue to add functions around it.

--- kmod/mimic.c
+++ kmod/mimic.c
@@ -1,23 +1,6 @@
-#include <crypto/chacha.h>
-#include <crypto/skcipher.h>
 #include <linux/bpf.h>
 #include <linux/btf.h>
-#include <linux/btf_ids.h>
-#include <linux/compiler_attributes.h>
-#include <linux/compiler_types.h>
-#include <linux/container_of.h>
-#include <linux/err.h>
-#include <linux/export.h>
-#include <linux/gfp_types.h>
-#include <linux/in.h>
-#include <linux/init.h>
-#include <linux/kref.h>
-#include <linux/module.h>
-#include <linux/random.h>
-#include <linux/scatterlist.h>
 #include <linux/skbuff.h>
-#include <linux/slab.h>
-#include <linux/stddef.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
 
@@ -67,87 +50,11 @@ __bpf_kfunc int mimic_change_csum_offset(struct __sk_buff* skb_bpf, __u16 new_pr
   return 0;
 }
 
-__bpf_kfunc void mimic_get_random_bytes(void* bytes__uninit, size_t bytes__uninit__sz) {
-  get_random_bytes(bytes__uninit, bytes__uninit__sz);
-}
-
-__bpf_kfunc struct mimic_skcipher_state* mimic_skcipher_init_state(const __u8* key,
-                                                                   size_t key__sz) {
-  struct mimic_skcipher_state* state;
-  int ret;
-
-  if (key__sz != CHACHA_KEY_SIZE) return NULL;
-  state = kzalloc(sizeof(*state), GFP_KERNEL);
-  if (!state) return NULL;
-  state->tfm = crypto_alloc_sync_skcipher("chacha20", 0, 0);
-  if (IS_ERR(state->tfm)) goto cleanup;
-  ret = crypto_sync_skcipher_setkey(state->tfm, key, key__sz);
-  if (ret < 0) goto cleanup;
-
-  kref_init(&state->refcount);
-  return state;
-
-cleanup:
-  if (state->tfm && !IS_ERR(state->tfm)) crypto_free_sync_skcipher(state->tfm);
-  kfree(state);
-  return NULL;
-}
-
-__bpf_kfunc struct mimic_skcipher_state* mimic_skcipher_acquire_state(
-  struct mimic_skcipher_state* state) {
-  return kref_get_unless_zero(&state->refcount) ? state : NULL;
-}
-
-static inline void mimic_skcipher_free_state(struct mimic_skcipher_state* state) {
-  crypto_free_sync_skcipher(state->tfm);
-  kfree(state);
-}
-
-static inline void mimic_skcipher_free_state_from_kref(struct kref* ref) {
-  struct mimic_skcipher_state* state = container_of(ref, struct mimic_skcipher_state, refcount);
-  mimic_skcipher_free_state(state);
-}
-
-// NOTE: Linux 6.8 uses CFI_NOSEAL for dtors; maybe backport is needed?
-// See https://lore.kernel.org/all/20231215092707.799451071@infradead.org/
-__bpf_kfunc void mimic_skcipher_release_state(struct mimic_skcipher_state* state) {
-  kref_put(&state->refcount, mimic_skcipher_free_state_from_kref);
-}
-
-// Encrypt or decrypt `data` in place.
-__bpf_kfunc int mimic_skcipher_crypt(struct mimic_skcipher_state* state, __u8* data,
-                                     size_t data__sz, union mimic_iv_repr* iv, bool encrypt) {
-  SYNC_SKCIPHER_REQUEST_ON_STACK(req, state->tfm);
-  struct scatterlist sg;
-  int ret;
-
-  sg_init_one(&sg, data, data__sz);
-  skcipher_request_set_sync_tfm(req, state->tfm);
-  skcipher_request_set_callback(req, 0, NULL, NULL);
-  skcipher_request_set_crypt(req, &sg, &sg, data__sz, iv);
-
-  if (encrypt) {
-    ret = crypto_skcipher_encrypt(req);
-  } else {
-    ret = crypto_skcipher_decrypt(req);
-  }
-
-  return 0;
-}
-
 __bpf_kfunc_end_defs();
-
-#define BTF_ID_FLAGS__MIMIC_CRYPTO_APIS                                      \
-  BTF_ID_FLAGS(func, mimic_get_random_bytes)                                 \
-  BTF_ID_FLAGS(func, mimic_skcipher_init_state, KF_ACQUIRE | KF_RET_NULL)    \
-  BTF_ID_FLAGS(func, mimic_skcipher_acquire_state, KF_ACQUIRE | KF_RET_NULL) \
-  BTF_ID_FLAGS(func, mimic_skcipher_release_state, KF_RELEASE)               \
-  BTF_ID_FLAGS(func, mimic_skcipher_crypt, KF_TRUSTED_ARGS)
 
 BTF_SET8_START(mimic_tc_set)
 BTF_ID_FLAGS(func, mimic_inspect_skb)
 BTF_ID_FLAGS(func, mimic_change_csum_offset)
-BTF_ID_FLAGS__MIMIC_CRYPTO_APIS
 BTF_SET8_END(mimic_tc_set)
 
 static const struct btf_kfunc_id_set mimic_tc_kfunc_set = {
@@ -155,29 +62,9 @@ static const struct btf_kfunc_id_set mimic_tc_kfunc_set = {
   .set = &mimic_tc_set,
 };
 
-BTF_SET8_START(mimic_xdp_set)
-BTF_ID_FLAGS__MIMIC_CRYPTO_APIS
-BTF_SET8_END(mimic_xdp_set)
-
-static const struct btf_kfunc_id_set mimic_xdp_kfunc_set = {
-  .owner = THIS_MODULE,
-  .set = &mimic_xdp_set,
-};
-
-BTF_ID_LIST(generic_dtor_ids)
-BTF_ID(struct, mimic_skcipher_state)
-BTF_ID(func, mimic_skcipher_release_state)
-
 static int __init mimic_init(void) {
-  const struct btf_id_dtor_kfunc generic_dtors[] = {{
-    .btf_id = generic_dtor_ids[0],
-    .kfunc_btf_id = generic_dtor_ids[1],
-  }};
-
   int ret = 0;
   ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_SCHED_CLS, &mimic_tc_kfunc_set);
-  ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_XDP, &mimic_xdp_kfunc_set);
-  ret = ret ?: register_btf_id_dtor_kfuncs(generic_dtors, ARRAY_SIZE(generic_dtors), THIS_MODULE);
   return ret;
 }
 
--- kmod/mimic.h
+++ kmod/mimic.h
@@ -2,24 +2,11 @@
 #define _MIMIC_KMOD_MIMIC_H
 
 #ifdef _MIMIC_KMOD
-#include <crypto/chacha.h>
-#include <crypto/skcipher.h>
 #include <linux/bpf.h>
-#include <linux/kref.h>
+#include <linux/skbuff.h>
 #elif defined _MIMIC_BPF
 #include "../bpf/vmlinux.h"
-#define CHACHA_IV_SIZE 16
 #endif
-
-struct mimic_skcipher_state {
-  struct crypto_sync_skcipher* tfm;
-  struct kref refcount;
-};
-
-union mimic_iv_repr {
-  __u8 bytes[CHACHA_IV_SIZE];
-  __u32 words[CHACHA_IV_SIZE / sizeof(__u32)];
-};
 
 #ifndef __ksym
 #define __ksym
@@ -27,12 +14,5 @@ union mimic_iv_repr {
 
 struct sk_buff* mimic_inspect_skb(struct __sk_buff* skb) __ksym;
 int mimic_change_csum_offset(struct __sk_buff* skb, __u16 protocol) __ksym;
-
-void mimic_get_random_bytes(void* bytes, size_t bytes_len) __ksym;
-struct mimic_skcipher_state* mimic_skcipher_init_state(const __u8* key, size_t key_len) __ksym;
-struct mimic_skcipher_state* mimic_skcipher_acquire_state(struct mimic_skcipher_state* state) __ksym;
-void mimic_skcipher_release_state(struct mimic_skcipher_state* state) __ksym;
-int mimic_skcipher_crypt(struct mimic_skcipher_state* state, __u8* data, size_t data_len,
-                         union mimic_iv_repr* iv, bool encrypt) __ksym;
 
 #endif  // _MIMIC_KMOD_MIMIC_H
