3a1e4f42690d8b3b6bf818f836cb4ae5e0734152
Eric Long <i@hack3r.moe>
2024-08-09 15:54:45 +0800

    bpf/vmlinux: fallback to kernel headers

--- bpf/vmlinux.h
+++ bpf/vmlinux.h
@@ -18,7 +18,17 @@
 #elif defined(_MIMIC_BPF_TARGET_ARCH_ppc64le) || defined(_MIMIC_BPF_TARGET_ARCH_powerpc64le)
 #include "vmlinux/ppc64le.h"  // IWYU pragma: export
 #else
-#include "vmlinux/system.h"  // IWYU pragma: export
+#include <asm/types.h>       // IWYU pragma: export
+#include <linux/bpf.h>       // IWYU pragma: export
+#include <linux/if_ether.h>  // IWYU pragma: export
+#include <linux/in.h>        // IWYU pragma: export
+#include <linux/ip.h>        // IWYU pragma: export
+#include <linux/ipv6.h>      // IWYU pragma: export
+#include <linux/stddef.h>    // IWYU pragma: export
+#include <linux/tcp.h>       // IWYU pragma: export
+#include <linux/types.h>     // IWYU pragma: export
+#include <linux/udp.h>       // IWYU pragma: export
+#include <stdbool.h>         // IWYU pragma: export
 #endif
 
 #endif  // _BPF_VMLINUX_H
