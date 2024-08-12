f8ad866e16043c59103ed5d4a4e044dde3b41cc7
Eric Long <i@hack3r.moe>
2024-08-12 22:52:46 +0800

    Fix control packet TCP flags

--- src/run.c
+++ src/run.c
@@ -198,7 +198,7 @@ static inline int send_ctrl_packet(struct conn_tuple* conn, __be32 flags, __u32 
                                    __u32 cwnd, const char* ifname) {
   struct send_options s = {
     .conn = *conn,
-    .flags = htons(ntohl(flags) >> 16),
+    .flags = ntohl(flags) >> 16,
     .seq = seq,
     .ack_seq = ack_seq,
     .cwnd = cwnd,
