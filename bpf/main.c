3cd2b183cd4b578e90f6f75186ffccc8273a9bb7
Eric Long <i@hack3r.moe>
2025-11-17 21:13:35 +0800

    fix: verifier error on Linux 6.1
    Commit e030dbada2d2e7bc70b406a3c34795f33b27db91 fixes verifier on Linux >= 6.15, but breaks on 6.1. Adding compat flags to revert this just on 6.1 and 6.6 (since it works before).
    
    Fixes https://github.com/hack3ric/mimic/issues/28.

--- bpf/main.c
+++ bpf/main.c
@@ -60,7 +60,11 @@ int store_packet(struct __sk_buff* skb, __u32 pkt_off, struct conn_tuple* key, i
   }
   if (has_remainder) {
     offset = i * SEGMENT_SIZE;
+#if defined(MIMIC_COMPAT_LINUX_6_1) || defined(MIMIC_COMPAT_LINUX_6_6)
+    __u32 copy_len = data_len - offset;
+#else
     __u32 copy_len = data_len % SEGMENT_SIZE;
+#endif
     if (copy_len > 0 && copy_len < SEGMENT_SIZE) {
       bpf_gt0_hack2(copy_len);
       packet = bpf_dynptr_data(&ptr, sizeof(*item) + offset, SEGMENT_SIZE);
